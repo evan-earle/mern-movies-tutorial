e5d22677a373cfee733a7f3b9fcd36718d2ef94c
Evan Earle <evan.earle@gmail.com>
2024-02-03 16:05:00 -0400

    edited schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,28 +1,32 @@
-import mongoose from 'mongoose'
-const {Schema} = mongoose;
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
-        type:String,
-        required:true
+      type: String,
+      required: true,
     },
     email: {
-        type:String,
-        required:true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type:String,
-        required:true
+      type: String,
+      required: true,
     },
     image: {
-        type:String,
-        default: {"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"}
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    watchlist:{
-        type:Array,
-        default: []
-    }
-}, {timestamps: true})
+    watchlist: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema)
+export default mongoose.model("User", userSchema);
