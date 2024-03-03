1e14d61d1849511a0bbe4d60b27b9f87a6e9a197
Evan Earle <evan.earle@gmail.com>
2024-03-03 10:45:00 -0400

    created routes with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { PopularMovies } from "./pages/PopularMovies";
+import { Watchlist } from "./pages/Watchlist";
+import { MovieDetails } from "./pages/MovieDetails";
+
 function App() {
-  return;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/popular" element={<PopularMovies />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/PopularMovies.jsx
@@ -0,0 +1,3 @@
+export const PopularMovies = () => {
+  return <div>Popular Movies</div>;
+};
